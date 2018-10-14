f11e1b14dfae881c1505bf22eba336b932863c81
Natalie Halawani <hala1224@gmail.com>
2018-10-14 18:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 /* Your code goes here */
 var customerName ='bob';
-function upperCaseCustomerName () {
-  return customerName.toUpperCase;
+// function upperCaseCustomerName () {
+//   return customerName.toUpperCase;
+// }
+// function setBestCustomer () {
+//   var bestCustomer = 'not bob';
+// }
+function upperCaseCustomerName() {
+  customerName=customerName.toUpperCase();
+  return customerName;
 }
-function setBestCustomer () {
-  var bestCustomer = 'not bob';
+function setBestCustomer() {
+  bestCustomer='not bob';
+}
+function overwriteBestCustomer() {
+  bestCustomer='maybe bob';
+}
+const leastFavoriteCustomer='Ted';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer='Jack';
 }
