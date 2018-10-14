ec3c7a704ddfa21ff77b851dbd7acb1bdb657a56
Natalie Halawani <hala1224@gmail.com>
2018-10-14 18:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 /* Your code goes here */
+var customerName ='bob';
+function upperCaseCustomerName () {
+  return customerName.toUpperCase;
+}
+function setBestCustomer () {
+  var bestCustomer = 'not bob';
+}
